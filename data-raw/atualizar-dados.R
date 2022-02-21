d5d9d123c6c4bb59f984ffb1f49d6a4219f41c00
Daniel Amaral <silva.daniel86@Gmail.com>
2022-02-21 00:25:10 -0300

    :rocket: primeira versão do pacote

---
+++ data-raw/atualizar-dados.R
@@ -0,0 +1,23 @@
+# Obter letras atualizadas
+artistas <- letras::obter_artistas()
+musicas <- tidyr::drop_na(letras::obter_musicas())
+letras <- tidyr::drop_na(letras::obter_letras())
+
+# Salvar letras no pacote
+usethis::use_data(artistas, overwrite = TRUE, compress = "xz")
+usethis::use_data(musicas, overwrite = TRUE, compress = "xz")
+usethis::use_data(letras, overwrite = TRUE, compress = "xz")
+
+# Salvar letras em txt
+purrr::walk(1:nrow(letras), function(ix_letra) {
+  letra <- letras[ix_letra,]
+  letra_txt <- glue::glue("{letra$musica}\n{letra$artista}\n\n{letra$letra}")
+  nome_arq <- paste0(gsub('[[:punct:]]+','', letra$musica), " - ",
+                     gsub('[[:punct:]]+','', letra$artista))
+  write.table(letra_txt, glue::glue("inst/txt/{nome_arq}.txt"),
+              append = FALSE)
+})
+
+# Salvar letras em txt zipadas
+letras_path <- Sys.glob(paths = "inst/txt/*.txt")
+zip::zip(zipfile = "inst/letras.zip", files = letras_path, mode = 'cherry-pick')
